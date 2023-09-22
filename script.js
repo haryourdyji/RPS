8f5acb5797409f43f8fbe393a915fc7ca5117fbc
Makinde Ayodeji <ayodejimakinde569@gmail.com>
2023-09-22 08:20:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const time = document.querySelector(".timer");
+// const imgChanger = document.querySelector(".brd-img");
+// const imgName = document.querySelector(".img-cont");
+// const score = document.querySelector(".score-update");
+// const winUpdate = document.querySelector(".result");
+
+// BUTTON
+
+// const btnStart = document.querySelector(".btn-str");
+// const btnQuit = document.querySelector(".btn-quut");
+// const btnToss = document.querySelector(".play");
+
+// btnStart.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   console.log("i got clicked");
+// });
